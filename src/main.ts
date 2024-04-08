119ed43699ad6e919b0cf986f7254624405ecb75
xuxb208 <xuxb208@tydic.com>
2024-04-08 18:12:17 +0800

    首页轮播实现

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import piniaStore from './store';
-
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import '@/styles/index.less';
 import '@/styles/reset.less';
 import 'uno.css';
@@ -25,8 +25,11 @@ themeService?.applyTheme(devuiDarkTheme);
 //vue3的挂载方式
 const app = createApp(App);
 
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
 app.use(router);
 app.use(piniaStore);
 app.use(Idux);
-
 app.mount('#app');
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,55 +1,164 @@
 <template>
   <div class="dark:text-slate-400 dark:bg-slate-900">
     <Header />
-    <main class="max-w-5xl px-4 mx-auto pb-22 sm:px-6 md:px-8 xl:px-12 xl:max-w-6xl">
-      <div class="pt-8 pb-7 sm:pb-8 sm:text-center">
-        <h1 class="relative mb-4 text-4xl tracking-tight font-blimone sm:text-5xl lg:text-6xl text-slate-900 dark:text-slate-200">
-          {{ appStore.h1 }}
-        </h1>
-        <p class="text-2xl text-slate-800 dark:text-slate-400">最新Vue3技术流，超全配置，大厂协作规范，大佬必备神器</p>
+    <main class="max-w-5xl px-4 mx-auto pb-22 sm:px-6 md:px-8 xl:px-12 xl:max-w-6xl carousel-box">
+      <div ref="listRef" class="carousel-list">
+        <div v-for="(el, index) in data" :key="index" class="carousel-item" @mousemove="eover()" @mouseout="eout">
+          <div class="card-header">
+            <span>{{ el.title }}</span>
+          </div>
+          <div class="content_area">
+            <el-image style="width: 100px; height: 100px" :src="el.avatar" :fit="fit" />
+            <p class="text item">{{ el.content }}</p>
+          </div>
+          <div class="footer">
+            <div>{{ el.date }}</div>
+            <div class="icon_status">
+              <div class="interaction">
+                <el-icon><View /></el-icon>{{ el.view }}
+              </div>
+              <div class="interaction">
+                <el-icon><ChatDotSquare /></el-icon>{{ el.chatDotSquare }}
+              </div>
+              <div class="interaction">
+                <el-icon><Pointer /></el-icon>{{ el.pointer }}
+              </div>
+              <div class="interaction">
+                <el-icon><Star /></el-icon>{{ el.star }}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
-    <article class="space-y-20 sm:space-y-32 md:space-y-40 lg:space-y-44">
-      <ul class="flex flex-wrap items-center justify-center py-6 sm:px-20 lg:px-36 xl:px-20 sm:justify-start lg:justify-start">
-        <li v-for="(item, index) in data" :key="index * 1.1" class="px-3 pt-4 md:px-4 sm:pt-5 md:pb-8">
-          <figure class="flex-none shadow-lg rounded-xl w-80 md:w-100">
-            <blockquote
-              class="px-6 py-8 text-lg font-semibold leading-8 bg-white rounded-t-xl md:p-5 md:text-base md:leading-8 text-slate-700 dark:text-slate-300 dark:bg-slate-800 dark:highlight-white/5"
-            >
-              <SvgIcon name="svg-marks" />
-              <p v-html="item.content"></p>
-            </blockquote>
-            <figcaption :class="`flex items-center p-6 space-x-4 leading-6 text-white md:px-10 md:py-6 rounded-b-xl ${item.color}`">
-              <div class="flex items-center justify-center flex-none bg-white rounded-full w-14 h-14">
-                <img :src="item.avatar" class="w-12 h-12 rounded-full" loading="lazy" />
-              </div>
-              <div class="flex-auto">
-                <div class="text-base font-semibold dark:text-slate-200">
-                  {{ item.title }}
-                  <p>{{ item.author }}</p>
-                </div>
-              </div>
-              <cite class="flex">
-                <a :href="item.github" class="transition-opacity duration-200 opacity-50 hover:opacity-75">
-                  <SvgIcon name="svg-github" />
-                </a>
-              </cite>
-            </figcaption>
-          </figure>
-        </li>
-      </ul>
-    </article>
   </div>
 </template>
 
 <script setup lang="ts">
-  import SvgIcon from '@/components/SvgIcon/index.vue';
-  import { useAppStore } from '@/store/modules/app';
+  // import SvgIcon from '@/components/SvgIcon/index.vue';
   import { framework } from './data';
   import Header from '@/components/Header/index.vue';
 
-  const appStore = useAppStore();
+  // @ts-ignore
+  import anime from 'animejs';
+
   const data = ref(framework);
+  ///动画相关代码
+  const listRef = ref();
+  let spacing = 10; //模块之间的间距
+  var animation = shallowRef<ReturnType<typeof anime>>(null);
+  const init = () => {
+    // 动画中所有模块实例对象集合
+    const children = listRef.value?.children || [];
+
+    //如果没有实例作拦截
+    if (!children.length) return;
+
+    // 获取列表集合中第一个元素宽，通过宽和元素之间的间距计算出移动距离firstDiff
+    const firstEl = children[0] as HTMLElement;
+    const firstDiff = firstEl.offsetWidth + spacing;
+
+    // 默认将list元素向左移动一个item的距离
+    listRef.value!.style.transform = `translateX(-${firstDiff + 40}px)`;
+
+    const transList: any = [];
+    let total = 0; //声明总宽
+    // 设置初始位置
+    anime.set(children, {
+      ['translateX']: (el: HTMLElement, i: number) => {
+        //计算得出总宽
+        const distance = el.offsetWidth + spacing;
+        total += distance;
+        //设置初始运动点为0
+        const diff = (i + 1) * distance;
+        //收集所有小模块运动x轴点值
+        transList[i] = { ['x']: diff };
+        return diff;
+      },
+    });
+    // 设置list容器的宽或高
+    listRef.value!.style['width'] = total + 'px';
+
+    // 添加动画
+    animation.value = anime({
+      targets: transList,
+      duration: 50000, //一个动画时间
+      easing: 'linear',
+      direction: 'horizontal',
+      ['x']: `+=${total}`,
+      loop: true, //是否循环
+      update: () => {
+        anime.set(children, {
+          ['translateX']: (_el: any, i: string | number) => {
+            return transList[i]['x'] % total;
+          },
+        });
+      },
+    });
+  };
+  onMounted(() => {
+    init();
+  });
+  //鼠标移入事件
+  const eover = () => {
+    animation.value.pause();
+  };
+  //鼠标移出事件
+  const eout = () => {
+    animation.value.play();
+  };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+  .carousel-box {
+    padding-top: 20px;
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    .carousel-item {
+      position: absolute;
+      top: 0;
+      left: 0;
+      max-width: 480px;
+      border-radius: 4px;
+      // border: 1px solid #e4e7ed;
+      background-color: rgba(255, 255, 255, 0.6);
+      color: #303133;
+      padding: 20px;
+      cursor: pointer;
+      .card-header {
+        font-weight: 800;
+      }
+      .content_area {
+        display: flex;
+        justify-items: center;
+        margin: 10px 0;
+        .el-image {
+          margin-right: 10px;
+        }
+        .text {
+          width: calc(100% - 100px);
+        }
+      }
+      .footer {
+        display: flex;
+        justify-content: space-between;
+        font-size: 12px;
+        margin-top: 10px;
+        .icon_status {
+          display: flex;
+        }
+        .interaction {
+          margin-right: 10px;
+          display: flex;
+          align-items: center;
+          cursor: pointer;
+          .el-icon {
+            margin-right: 3px;
+          }
+        }
+      }
+    }
+  }
+</style>
